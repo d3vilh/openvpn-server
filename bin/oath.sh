05a8e072a411d858503faf71df43dc60a301b2e1
Mr. Φίλιππος <33333559+d3vilh@users.noreply.github.com>
2023-09-28 22:34:39 +0300

    script updates

--- bin/oath.sh
+++ bin/oath.sh
@@ -9,10 +9,16 @@ mkdir -p /etc/openvpn/log
 PASSFILE=$1    # Password file passed by openvpn-server with "auth-user-pass-verify /opt/app/bin/oath.sh via-file" in server.conf
 OPENVPN_DIR=/etc/openvpn
 OATH_SECRETS=$OPENVPN_DIR/clients/oath.secrets
-LOG_FILE=$OPENVPN_DIR/log/oath.log
+LOG_FILE=/var/log/openvpn/oath.log
+
+
+echo "passfile: $PASSFILE"
+echo "openvpn_dir: $OPENVPN_DIR"
+echo "oath_secrets: $OATH_SECRETS"
+echo "log_file: $LOG_FILE"
 
 echo "$(date) - PASSFILE: $PASSFILE" | tee -a $LOG_FILE
-echo $PASSFILE >> $LOG_FILE
+cat $PASSFILE >> $LOG_FILE
 
 # Geting user and password from passed by OpenVPN server tmp file
 user=$(head -1 $PASSFILE)
@@ -34,8 +40,11 @@ echo "$(date) - Code: $code" | tee -a $LOG_FILE
 
 if [ "$code" = "$pass" ];
 then
-    echo "$(date) - Authentication succeeded for user $user" | tee -a $LOG_FILE
-	exit 0
+    echo "OK"
+   # echo "$(date) - Authentication succeeded for user $user" | tee -a $LOG_FILE
+        exit 0
+else 
+echo "FAIL"
 fi
 
 # See if we have password and MFA, or just MFA
@@ -46,14 +55,14 @@ echo "$(date) - Password: $pass" | tee -a $LOG_FILE
 
 if [ $? -eq 0 ];
 then
-	realpass=$(echo "$pass" | cut -d: -f1)
-	mfatoken=$(echo "$pass" | cut -d: -f2)
+        realpass=$(echo "$pass" | cut -d: -f1)
+        mfatoken=$(echo "$pass" | cut -d: -f2)
 
-	echo "$(date) - Real password: $realpass" | tee -a $LOG_FILE
-	echo "$(date) - MFA token: $mfatoken" | tee -a $LOG_FILE
+        echo "$(date) - Real password: $realpass" | tee -a $LOG_FILE
+        echo "$(date) - MFA token: $mfatoken" | tee -a $LOG_FILE
 
-	# put code here to verify $realpass, the code below the if validates $mfatoken or $pass if false
-	# exit 0 if the password is correct, the exit below will deny access otherwise
+        # put code here to verify $realpass, the code below the if validates $mfatoken or $pass if false
+        # exit 0 if the password is correct, the exit below will deny access otherwise
 fi
 
 # If we make it here, auth hasn't succeeded, don't grant access
